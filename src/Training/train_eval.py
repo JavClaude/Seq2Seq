b2bea6449a92f9d8ff86b67050d06c619b623383
Javclaude <pierre.arthur.claude@outlook.fr>
2021-04-12 22:31:15 +0200

    Update train eval functions

--- src/Training/train_eval.py
+++ src/Training/train_eval.py
@@ -1,5 +1,6 @@
 import tqdm
 import torch
+import mlflow
 import logging
 
 logger = logging.getLogger(__name__)
@@ -23,10 +24,15 @@ def train_model(Model, Optimizer, Criterion, Dataset, global_train_it):
         loss = Criterion(preds[:, 1:].transpose(2, 1), seq_out[:, 1:])
         loss.backward()
 
+        if local_train_it % 400 == 0:
+            print("Training loss: {} at {}".format(loss.item(), local_train_it))
+
         Optimizer.step()
         Model.zero_grad()
 
         epoch_loss += loss.item()
+        mlflow.log_metric("Training loss", loss.item(), global_train_it)
+
         local_train_it += 1
         global_train_it += 1
 
@@ -49,8 +55,12 @@ def eval_model(Model, Criterion, Dataset, global_eval_it):
             preds = Model(seq_in, seq_in_length, seq_out)
 
             loss = Criterion(preds[:, 1:].transpose(2, 1), seq_out[:, 1:])
+            if local_eval_it % 400 == 0:
+                print("Eval loss: {} at {}".format(loss.item(), local_eval_it))
 
             epoch_loss += loss.item()
+            mlflow.log_metric("Eval loss", loss.item(), global_eval_it)
+
             local_eval_it += 1
             global_eval_it += 1
 
